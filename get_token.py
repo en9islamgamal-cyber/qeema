08f7681287998dfce9752bc9d79b507f07f52432
en9islamgamal-cyber <en9islamgamal@gmail.com>
2026-04-22 01:42:04 +0300

    Delete

--- get_token.py
+++
@@ -1,6 +0,0 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-import json
-
-flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", ["https://www.googleapis.com/auth/youtube.upload"])
-creds = flow.run_local_server(port=0)
-print(json.dumps({"refresh_token": creds.refresh_token}))
